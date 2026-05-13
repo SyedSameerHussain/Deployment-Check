93747821c8de410f32d088d8281832287da9a071
Sameer <syedsameershah2008@gmail.com>
2026-05-14 04:09:20 +0500

    first

---
+++ Backend/app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+config({ path: ".env" });
+
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://zeelab.vercel.app",
+        "https://zeelab-dashboard.vercel.app"
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+console.log("hello backend")
+app.use((req, res, next) => {
+    console.log("REQ:", req.method, req.url, "ORIGIN:", req.headers.origin);
+    next();
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/env-check", (req, res) => {
+    res.json({
+        FRONTEND_URI: process.env.FRONTEND_URI,
+        DASHBOARD_URI: process.env.DASHBOARD_URI,
+        PRODUCTION_URL: process.env.PRODUCTION_URL,
+        PRODUCTION_DASHBOARD_URI:
+            process.env.PRODUCTION_DASHBOARD_URI,
+    });
+});
+
+
+
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome"
+    })
+})
+dbConnection();
+
+export default app;
