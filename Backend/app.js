626ad4272147b307bb053732c336248c7562ebcf
RoadToRight <syedsameershah0786@gmail.com>
2026-05-16 06:22:20 -0700

    imporve

--- Backend/app.js
+++ Backend/app.js
@@ -4,12 +4,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const corsOptions = {
-    origin: "https://cozy-florentine-857e6c.netlify.app",
-    credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
